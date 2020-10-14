07a75ef75bcce0c50d7f301b53d5730b8543d3f9
pixel <pixel@DESKTOP-L3CC1RT>
2020-10-14 17:07:47 +0200

    Add project files.

---
+++ Point/Point.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Point
+    {
+        //Itt propretikkel oldjuk meg a privát változókat. Ez a PropFull
+        private int _x, _y;
+
+        public int X
+        {
+            get { return _x;  }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y;  }
+            set { _y = value; }
+        }
+
+        //Síknegyed = quadrant
+        public int Quadrant
+        {
+            get
+            {
+                if (_x >= 0)
+                {
+                    if (_y >= 0)
+                        return 1;
+                    else
+                        return 4;
+                }
+                else
+                {
+                    if (_y >= 0)
+                        return 2;
+                    else
+                        return 3;
+                }
+            }
+            set
+            {
+                if(value > 0 && value < 5)
+                {
+                    _x = Math.Abs(_x);
+                    _y = Math.Abs(_y);
+                    switch (value)
+                    {
+                        case 1: break;
+                        case 2: _x *= -1; break;
+                        case 3: _x *= -1; _y *= -1; break;
+                        case 4: _y *= -1; break;
+                    }
+                }
+            }
+        }
+
+        //Tömörített prop, privát változó nélkül. Property nevek nagybetűvel kezdődnek.
+        //public int Y { get; set; }
+
+        //Ctrl+. -> Generate Constructor
+        //Az üres paraméterlistás konstruktor alapértelmezett, de üres.
+        //Ez nekünk legyen az origó értéke.
+        public Point()
+        {
+            this._x = 0;
+            this._y = 0;
+        }
+
+        //Lehet más construktort is generálni.
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Console.WriteLine("Új pont: " + this.ToString());
+        }
+
+        //Lehetnek metódusai egy osztálynak
+        public double Distance()
+        {
+            double dis = Math.Sqrt(Math.Pow(_x, 2) + Math.Pow(_y, 2));
+            Console.WriteLine(ToString() + " távolsága az origótól: " + dis);
+            return dis;
+        }
+
+        public double Distance(Point b)
+        {
+            double dis = Math.Sqrt(Math.Pow(this._x - b._x, 2) + Math.Pow(this._y - b._y, 2));
+            Console.WriteLine(ToString() + " távolsága a " + b.ToString() + " ponttól: " + dis);
+            return dis;
+        }
+
+        //És lehet osztályszintű metódus is.
+        public static double Distance(Point a, Point b)
+        {
+            double dis = Math.Sqrt(Math.Pow(a._x - b._x, 2) + Math.Pow(a._y - b._y, 2));
+            Console.WriteLine(  "A " + a.ToString() +
+                                " és " + b.ToString() +
+                                " pontok közötti távolság: " + dis);
+            return dis;
+        }
+
+        //toString - az elem kiírása
+        //Ctr+. -> Generate Overrides
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("(x:").Append(_x);
+            sb.Append(", y:").Append(_y);
+            sb.Append(", Q:");
+            switch (Quadrant)
+            {
+                case 1: sb.Append("I.");    break;
+                case 2: sb.Append("II.");   break;
+                case 3: sb.Append("III.");  break;
+                case 4: sb.Append("IV.");   break;
+            }
+            sb.Append(')');
+            return sb.ToString();
+        }
+
+        //2 elem mikor egyenlő.
+        //Egyes halmazok a HashCode után ezt használják az egyenlőség meghatározására.
+        //Ez lehet pl. halmazoknál. Valami csak hashcode, de valami ezt is megnézi már.
+        public override bool Equals(object obj)
+        {
+            /*
+            return obj is Point point &&
+                   x == point._x &&
+                   y == point._y;
+            */
+            return base.Equals(obj);
+        }
+
+        //Ha már más bemeneti argumentumlista van, akkor egyedi metódus.
+        //Most a név is más, hogy könnyen hivatkozhassam a test-ben. Így nem override!
+        public bool equals(Point point)
+        {
+            return point._x == this._x &&
+                   point._y == this._y;
+        }
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Origó objektum
+			Point o = new Point();
+			//2 pont a síkon
+			Point a = new Point(3, 4);
+			Point b = new Point(-5, 10);
+			//Origótól vett távolság
+			o.Distance();
+			a.Distance();
+			b.Distance();
+			//A távolsága az origótól - megeggyezik:
+			a.Distance(o);
+			//B távolsága A-tól
+			b.Distance(a);
+			Point.Distance(b, a);
+			//Új C pont, majd A és C távolsága:
+			Point c = new Point(-4, -3);
+			Point.Distance(a, c);
+
+			//Új D pont.
+			Point d = new Point();
+			//A D pontot egyenlővé teszem a-val.
+			if (d.X == 0)
+			{
+				d.X = 3;
+			}
+			if (d.Y == 0)
+			{
+				d.Y = 4;
+			}
+            //Equals override nélkül nem egyenlő
+            //Új equals definiálva egyenlő.
+            //Általános equals-t kiegészítve már egyenlő.
+            Console.WriteLine("---EQUALS---");
+            Console.WriteLine("Az a" + a.ToString() + " és d" + b.ToString() + " pontok egyenlősége:");
+            Console.WriteLine(a.Equals(d));
+            Console.WriteLine(a.equals(d));
+
+			//Special Property
+			Console.WriteLine("---PROP---");
+            Console.WriteLine("Az A pont:" + a.ToString());
+            Console.WriteLine("Az A pont síknegyede: " + a.Quadrant);
+			a.Quadrant = 2;
+			Console.WriteLine("Az A pont a II. síknegyedben: " + a.ToString());
+			a.Quadrant = 3;
+			Console.WriteLine("Az A pont a III. síknegyedben: " + a.ToString());
+			a.Quadrant = 4;
+			Console.WriteLine("Az A pont a IV. síknegyedben: " + a.ToString());
+		}
+    }
+}
